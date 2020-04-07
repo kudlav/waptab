6d6950ef5ebcfd40cf58788d207c190f6a6179d7
Hoang Anh Le <xlehoa00@vutbr.cz>
2020-04-07 12:48:43 +0200

    rss widget

--- react/app/widget/rss/widget.js
+++ react/app/widget/rss/widget.js
@@ -4,13 +4,49 @@
  */
 
 import React, { Component } from 'react';
+import * as rssParser from 'react-native-rss-parser';
+import './widget.scss'
 
 export default class RssWidget extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {channel: [],ready: false};
+	  }
+
+	componentDidMount() {
+		const RSS_URL = 'http://www.nasa.gov/rss/dyn/breaking_news.rss';
+		return fetch('https://cors-anywhere.herokuapp.com/'+RSS_URL)
+		.then((response) => response.text())
+		.then((responseData) => rssParser.parse(responseData))
+		.then((rss) => {
+		this.setState({channel: rss,ready:true});
+		});
+	}
+
 	render() {
-		return (
-			<p>
-				Nadržený beran přeskočil ohradu a během 24 hodin zplodil 33 potomků. [Novinky.cz]
-			</p>
-		);
+		if(this.state.ready)
+		{
+				const items = this.state.channel.items.map((item, key) =>
+					<div className={"itemContainer"}>
+						<div className={"itemTitle"}>{item.title}</div>
+						<div className={"itemDesc"}>{item.description}</div>
+					</div>
+				);
+
+			console.log(this.state.channel.items);
+			return (
+				<div className={"rssWidget"}>
+				{items}
+				</div>
+				);
+		}
+		else
+		{
+			return (
+				<p>
+					Načítání...
+				</p>
+			);
+		}
 	}
 }
